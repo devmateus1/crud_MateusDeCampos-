f9326aec8a74ab1f38435b09d53b706339bc66f6
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-26 20:04:37 -0300

    alterações

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'root'
 MYSQL_DATABASE = 'MateusTurkoco_db1'
+
+
+
+#para rodar o projeto nao posso esquecer deste codigo
+#pipinstall mysql-connector-python
---
+++ crud.py
@@ -0,0 +1,51 @@
+import mysql.connector
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
+
+def get_connection():
+    return mysql.connector.connect(
+        host = MYSQL_HOST,
+        user = MYSQL_USER,
+        password = MYSQL_PASSWORD,
+        database = MYSQL_DATABASE
+    )
+
+def create_user(nome, telefone, email, usuario, senha):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "insert usuario(nome, telefone, email, usuario, senha) VALUES(%s, %s, %s, %s, %s)"
+    cursor.execute(query, [nome, telefone, email,usuario, senha])
+    cursor.commit()
+    cursor.close()
+    conn.close()
+
+
+def read_users():
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM usuario)"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+
+
+def update_user(user_id, nome, telefone, email, usuario, senha):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "UPDATE usuario SET nome=%s telefone=%s, email=%s, usuario=%s, senha=%s WHERE idusuario =%s"
+    cursor.execute(query, [user_id, nome, telefone, email,usuario, senha])
+    cursor.commit()
+    cursor.close()
+    conn.close()
+
+
+def delete_user(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "DELETE FROM usuario WHERE idusuario = %s"
+    cursor.execute(query, user_id)
+    cursor.commit()
+    cursor.close()
+    conn.close()
+
